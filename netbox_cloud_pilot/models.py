cb2a005b5fc050d07834f589ab3f1fc5f117a182
Abhimanyu Saharan <asaharan@onemindservices.com>
2024-01-06 03:08:48 +0530

    fixed license validation error

--- netbox_cloud_pilot/models.py
+++ netbox_cloud_pilot/models.py
@@ -95,9 +95,7 @@ class NetBoxConfiguration(JobsMixin, PrimaryModel):
                 raise ValidationError(e)
 
         if self.license:
-            if not self.license.startswith("ghp_"):
-                raise ValidationError({"license": "Invalid license key"})
-            elif not self.license.startswith("github_pat_"):
+            if not self.license.startswith("ghp_") and not self.license.startswith("github_pat_"):
                 raise ValidationError({"license": "Invalid license key"})
 
     @lru_cache(maxsize=2)
