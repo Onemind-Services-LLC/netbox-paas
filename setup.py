0ea9a94399bd00e784376a2df1d2f6fac39fce78
Abhimanyu Saharan <asaharan@onemindservices.com>
2024-01-23 18:06:36 +0530

    Update setup.py
    Signed-off-by: Abhimanyu Saharan <asaharan@onemindservices.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme) as fh:
 
 setup(
     name="netbox-cloud-pilot",
-    version="0.1.16",
+    version="0.1.17",
     description="Enhances NetBox on CloudMyDC's VAP with advanced management and control features.",
     long_description=long_description,
     long_description_content_type="text/markdown",
